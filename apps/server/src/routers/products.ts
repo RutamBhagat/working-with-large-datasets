aebef6b9360189368c7b784fc309de5ea5eb7753
Rutam Bhagat <rutambhagat@gmail.com>
2026-01-13 00:40:41 +0530

    feat: pagination

--- apps/server/src/routers/products.ts
+++ apps/server/src/routers/products.ts
@@ -1,5 +1,8 @@
-import { db } from "@working-with-large-datasets/db";
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { asc, db, gt } from "@working-with-large-datasets/db";
+import { products } from "@working-with-large-datasets/db/schema";
 
 export const productsRouter = new Hono();
 
@@ -77,3 +80,79 @@ productsRouter.get("/sort-by-name", async (c) => {
     );
   }
 });
+
+productsRouter.get(
+  "/offset-and-limit",
+  zValidator(
+    "query",
+    z.object({
+      offset: z.coerce.number().default(0),
+      limit: z.coerce.number().default(10),
+    })
+  ),
+  async (c) => {
+    try {
+      const { offset, limit } = c.req.valid("query");
+
+      const products = await db.query.products.findMany({
+        limit,
+        offset,
+      });
+
+      if (products.length === 0) {
+        return c.json(
+          {
+            success: false,
+            data: { error: "No data found", message: "No data found" },
+          },
+          404
+        );
+      }
+
+      return c.json({ success: true, data: products });
+    } catch (error) {
+      return c.json(
+        { success: false, data: { error, message: "Internal server error" } },
+        500
+      );
+    }
+  }
+);
+
+productsRouter.get(
+  "/cursor-pagination",
+  zValidator(
+    "query",
+    z.object({
+      limit: z.coerce.number().default(10),
+      cursor: z.coerce.number().default(0),
+    })
+  ),
+  async (c) => {
+    try {
+      const { cursor, limit } = c.req.valid("query");
+      const productResult = await db
+        .select()
+        .from(products)
+        .limit(limit)
+        .where(cursor ? gt(products.id, cursor) : undefined)
+        .orderBy(asc(products.id));
+
+      if (productResult.length === 0) {
+        return c.json(
+          {
+            success: false,
+            data: { error: "No data found", message: "No data found" },
+          },
+          404
+        );
+      }
+      return c.json({ success: true, data: productResult });
+    } catch (error) {
+      return c.json(
+        { success: false, data: { error, message: "Internal server error" } },
+        500
+      );
+    }
+  }
+);
--- apps/server/src/routers/users.ts
+++ apps/server/src/routers/users.ts
@@ -1,3 +1,30 @@
+import { db } from "@working-with-large-datasets/db";
 import { Hono } from "hono";
 
 export const usersRouter = new Hono();
+
+usersRouter.get("/last-10-users", async (c) => {
+  try {
+    const users = await db.query.users.findMany({
+      orderBy: { id: "desc" },
+      limit: 10,
+    });
+
+    if (users.length === 0) {
+      return c.json(
+        {
+          success: false,
+          data: { error: "No data found", message: "No data found" },
+        },
+        404
+      );
+    }
+
+    return c.json({ success: true, data: users });
+  } catch (error) {
+    return c.json(
+      { success: false, data: { error, message: "Internal server error" } },
+      500
+    );
+  }
+});
