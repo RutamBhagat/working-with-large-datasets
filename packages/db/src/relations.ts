af7ff10e0024bc93909489d1ff9396f7a9c01f8a
Rutam Bhagat <rutambhagat@gmail.com>
2026-01-12 21:39:35 +0530

    feat: add database schema and relations for users, products, and orders; include migration files

--- packages/db/src/relations.ts
+++ packages/db/src/relations.ts
@@ -2,5 +2,20 @@ import { defineRelations } from "drizzle-orm";
 import * as schema from "./schema";
 
 export const relations = defineRelations(schema, (r) => ({
-  users: {},
+  users: {
+    orders: r.many.orders(),
+  },
+  products: {
+    orders: r.many.orders(),
+  },
+  orders: {
+    user: r.one.users({
+      from: r.orders.userId,
+      to: r.users.id,
+    }),
+    product: r.one.products({
+      from: r.orders.productId,
+      to: r.products.id,
+    }),
+  },
 }));
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,7 +1,28 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  varchar,
+  integer,
+  boolean,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
+  id: serial().primaryKey(),
+  firstName: varchar("first_name"),
+  lastName: varchar("last_name"),
+});
+
+export const orders = pgTable("orders", {
+  id: serial().primaryKey(),
+  paid: boolean(),
+  userId: integer("user_id").references(() => users.id),
+  productId: integer("product_id").references(() => products.id),
+});
+
+export const products = pgTable("products", {
+  id: serial().primaryKey(),
+  name: varchar(),
+  department: varchar(),
+  price: integer(),
+  weight: integer(),
 });
