7023e5e04c0824e5db12719ca5c726a5e883d5b8
Rutam Bhagat <rutambhagat@gmail.com>
2026-01-12 21:32:55 +0530

    chore: drizzle v1 to v2

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,6 @@
 import { env } from "@working-with-large-datasets/env/server";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-import * as schema from "./schema";
+import { relations } from "./relations";
 
-export const db = drizzle(env.DATABASE_URL, { schema });
+export const db = drizzle(env.DATABASE_URL, { relations: relations });
